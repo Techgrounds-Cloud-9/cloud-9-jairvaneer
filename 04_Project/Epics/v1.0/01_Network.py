6a3bafddf12e50a5d9eb6b6ed1e7b19b9a6f3f97
Jaïr van Eer <jairvaneer@gmail.com>
2022-12-05 16:56:06 +0100

    Ready for the VPC code stack

--- 04_Project/Epics/v1.0/01_Network.py
+++ 04_Project/Epics/v1.0/01_Network.py
@@ -1,13 +1,3 @@
 # This IaC code contains the complete network stack and all its components
 # The first part wil provision the VPC with two public subnets
 # After this the components will be added
-
-vpc = ec2.Vpc(
-        self, "JairVpc"
-        cidr="10.10.10.0/24"
-        max_azs=2
-        nat_gateways=0
-        subnet_configuration= [
-            ec2.SubnetConfiguration(name="public", cidr_mask=24, subnet_type=ec2.SubnetType.Public)
-        ]
-)
